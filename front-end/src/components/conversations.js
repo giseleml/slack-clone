1d914ffe40e22290934e4ae5266a2bc8365a7817
giseleml <giselemaraleo@gmail.com>
2019-03-13 17:50:05 -0300

    Adding Send Message Component

--- front-end/src/components/conversations.js
+++ front-end/src/components/conversations.js
@@ -3,15 +3,35 @@ import React from 'react'
 export const Conversations = props => {
   return(
     <div className="conversation-pannel">
-      {
-        props.messageData.map(msg => {
-          return (
-              <div key={msg.id}>{msg.messages.map(i => {
-                return <p><b>User says:</b> {i.text}</p>
-              })}
-              </div>
-        )})
-      }
+      <ReceivedMessages messageData={props.messageData}/>
+      <SendMessages />
+    </div>
+  )
+}
+
+const ReceivedMessages = props => {
+  return(
+    <div className="received-msg">
+    {
+      props.messageData.map(msg => {
+        return (
+            <div key={msg.id}>{msg.messages.map(i => {
+              return <p><b>User says:</b> {i.text}</p>
+            })}
+            </div>
+      )})
+    }
+    </div>
+  )
+}
+
+const SendMessages = () => {
+  return(
+    <div className="send-msg">
+      <div>
+        <button>SEND</button>
+        <input type="text" id="type-msg"></input>
+      </div>
     </div>
   )
 }
